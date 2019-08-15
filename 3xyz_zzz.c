a814ccff18b224886895701e8a99e142cef1854e
sathish Jarugumalli <jsathishkumarc@gmail.com>
2019-08-15 18:48:26 +0530

    Create 3xyz_zzz.c

---
+++ 3xyz_zzz.c
@@ -0,0 +1,16 @@
+XYZ + XYZ +XYZ = YYY
+
+We can assume 10 possible combination of numbers with same digits. It can be either 000 or 111 or 222 or 333 or 444 or 555 or 666 or 777 or 888 or 999
+
+The above equitation can be written as, 3 x (XYZ) = YYY
+
+Thus if we divide each of the above possible 3 digit combination by number 3, there must be a number which will have the same middle digit repeating 3 times. Therefore, dividing each of the above three digit numbers by 3 you will get, 0, 37, 74, 111, 148, 185, 222, 259, 296, 333 respectively
+
+Now, The only solution wherein the “middle digit”, i.e, Y is repeating in the answer is 148. Here you can see, the middle digit 4 in 148 is repeated in the answer, when you multiply by 3 [i.e 3X 148 = 444]
+
+Similary, if you have a question XYZ + XYZ + XYZ = ZZZ
+
+i.e, 3 XYZ = ZZZ
+
+The only solution wherein the “end digit”, i.e, Z is repeating in the answer is 185. Here you can see, the end digit 5 in 185 is repeated in the answer, when you multiply by 3 i.e, 3 X 185 = 555
+s
